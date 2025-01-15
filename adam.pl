303420752c9c1ac57fda454893413849b08a0624
AdSchl2E <89904512+AdSchl2E@users.noreply.github.com>
2025-01-15 10:16:04 +0100

    Update adam.pl

---
+++ adam.pl
@@ -0,0 +1,37 @@
+%%%%%%%%%%%%%%%%%%%%%%
+%%% IHM
+%%%%%%%%%%%%%%%%%%%%%%
+
+% Affiche le plateau de jeu
+output_board(Board) :-
+    nl,
+    output_rows(Board, 6), % On commence par la 6ème ligne (le bas)
+    nl.
+
+% Affiche les lignes du bas vers le haut
+output_rows(_, 0) :- !.
+output_rows(Board, Row) :-
+    output_row(Board, Row),
+    nl,
+    NextRow is Row - 1,
+    output_rows(Board, NextRow).
+
+% Affiche une ligne donnée
+output_row(Board, Row) :-
+    write('|'),
+    output_cells(Board, Row, 1).
+
+% Parcourt les colonnes et affiche la cellule correspondante
+output_cells(_, _, 8) :-
+    nl, !.
+output_cells(Board, Row, Col) :-
+    nth1(Col, Board, Column), % Récupère la colonne actuelle
+    nth1(Row, Column, Cell), % Récupère la cellule (contenu) à la ligne Row
+    output_square(Cell), % Affiche le contenu de la cellule
+    write('|'),
+    NextCol is Col + 1,
+    output_cells(Board, Row, NextCol).
+
+% Affiche une cellule (X, O, ou vide)
+output_square(Cell) :-
+    (var(Cell) -> write(' '); write(Cell)), !. % Affiche un espace si la cellule est vide, sinon affiche le contenu (X, O, etc.)
