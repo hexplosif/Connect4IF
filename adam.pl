ab9220c9dc2a475c6aef3b68190e180b769a74ec
AdSchl2E <89904512+AdSchl2E@users.noreply.github.com>
2025-01-15 10:33:16 +0100

    V0.1 - À tester

--- adam.pl
+++ adam.pl
@@ -5,12 +5,38 @@
 /* 
 nth1(i, liste, element)
 Ça met dans élément la ième valeur de la liste
+
+nl c'est new line (\n)
+
+Faut que le Board ce soit :
+
+[[x, o, _, _, _, _], 
+ [x, x, _, _, _, _], 
+ [o, o, _, _, _, _], 
+ [_, _, _, _, _, _], 
+ [o, _, _, _, _, _], 
+ [x, _, _, _, _, _], 
+ [_, _, _, _, _, _]].
+
+Et ça affiche :
+
+| | | | | | | |
+| | | | | | | |
+| | | | | | | |
+| | | | | | | |
+|o|x|o| | | | |
+|x|x|o| |o|x| |
+---------------
+ 1 2 3 4 5 6 7
+
 */
 
 % Affiche le plateau de jeu
 output_board(Board) :-
     nl,
-    output_rows(Board, 6), % On commence par la 6ème ligne (le bas)
+    output_rows(Board, 6), % On commence par la 6ème ligne (le bas) sinon ça s'affiche à l'envert
+    nl,
+    output_column_numbers, % Affiche les numéros de colonnes
     nl.
 
 % Affiche les lignes du bas vers le haut
@@ -40,3 +66,8 @@ output_cells(Board, Row, Col) :-
 % Affiche une cellule (X, O, ou vide)
 output_square(Cell) :-
     (var(Cell) -> write(' '); write(Cell)), !. % Affiche un espace si la cellule est vide, sinon affiche le contenu (X, O, etc.)
+
+% Affiche les numéros des colonnes (1 à 7)
+output_column_numbers :-
+    write('---------------'), nl.
+    write(' 1 2 3 4 5 6 7'), nl.
