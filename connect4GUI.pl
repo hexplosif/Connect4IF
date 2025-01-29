d8456fc32b9516e4fab078cbab2da5a97209bb08
mbonkoungou <153067288+mbonkoungou@users.noreply.github.com>
2025-01-29 08:25:36 +0100

    Update connect4GUI.pl
    Win function done

--- connect4GUI.pl
+++ connect4GUI.pl
@@ -270,18 +270,64 @@ extract_column([], [], []).
 extract_column([[H | T] | Rows], [H | Column], [T | RestRows]) :-
     extract_column(Rows, Column, RestRows).
 
-horizontal_win(B, M) :- 
-    member(Row, B),
+horizontal_win(Board, M) :- 
+    member(Row, Board),
     isconsecutive(Row, M, 4).
 
-vertical_win(B, M) :- 
-    transpose(B, TB),
-    member(Row, TB),
+vertical_win(Board, M) :- 
+    transpose(Board, TBoard),
+    member(Row, TBoard),
     isconsecutive(Row, M, 4).
+
+diagonal_win_check(Board, M) :-
+    length(Board, Rows),
+    length(Board, Cols),
+    RowStartMax is Rows - 3,
+    ColStartMax is Cols - 3,
+    between(0, RowStartMax, RowStart),
+    between(0, ColStartMax, ColStart),
+    diagonal_win_from(Board, RowStart, ColStart, M).
+
+diagonal_win_check(Board, M) :-
+    length(Board, Rows),
+    length(Board, Cols),
+    RowStartMax is Rows - 3,
+    ColStartMax is Cols - 3,
+    between(0, RowStartMax, RowStart),
+    ColEndMax is Cols - 1,
+    between(0, ColEndMax, ColEnd),
+    diagonal_win_from_reverse(Board, RowStart, ColEnd, M).
+
+diagonal_win_from(Board, RowStart, ColStart, M) :-
+    nth0(RowStart, Board, Row1),
+    nth0(ColStart, Row1, M),
+    Row2Start is RowStart + 1, Col2Start is ColStart + 1,
+    nth0(Row2Start, Board, Row2),
+    nth0(Col2Start, Row2, M),
+    Row3Start is Row2Start + 1, Col3Start is Col2Start + 1,
+    nth0(Row3Start, Board, Row3),
+    nth0(Col3Start, Row3, M),
+    Row4Start is Row3Start + 1, Col4Start is Col3Start + 1,
+    nth0(Row4Start, Board, Row4),
+    nth0(Col4Start, Row4, M).
+
+diagonal_win_from_reverse(Board, RowStart, ColEnd, M) :-
+    nth0(RowStart, Board, Row1),
+    nth0(ColEnd, Row1, M),
+    Row2Start is RowStart + 1, Col2Start is ColEnd - 1,
+    nth0(Row2Start, Board, Row2),
+    nth0(Col2Start, Row2, M),
+    Row3Start is Row2Start + 1, Col3Start is Col2Start - 1,
+    nth0(Row3Start, Board, Row3),
+    nth0(Col3Start, Row3, M),
+    Row4Start is Row3Start + 1, Col4Start is Col3Start - 1,
+    nth0(Row4Start, Board, Row4),
+    nth0(Col4Start, Row4, M).
 
 win(B, M) :- 
     horizontal_win(B, M);
     vertical_win(B, M).
+    diagonal_win_check(Board, M).
 
 %.......................................
 % game_over
